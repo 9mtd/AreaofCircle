07f89a084f39647a7542d6499bb063c72192a361
9mtd <aduyend@gmail.com>
2021-07-14 22:01:26 -0500

    Add project files.

---
+++ AreaofCircle/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaofCircle
+{
+    public class Circle
+    {
+ 
+        public static double areaMethod(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double circumferenceMethod(double radius)
+        {
+            return Math.PI * 2 * radius;
+        }
+    }
+    
+}
---
+++ AreaofCircle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaofCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = -1;
+
+            while (radius <= 0)
+            {
+                Console.WriteLine("Enter a radius");
+                string radiusInput = Console.ReadLine();
+                var text = double.TryParse(radiusInput, out radius);
+                if (!text || text && radius < 0)
+                {
+                    Console.WriteLine("Not a valid radius!");
+                }
+            }
+
+            double area = Circle.areaMethod(radius);
+            double circumference = Circle.circumferenceMethod(radius);
+
+
+            Console.WriteLine("Area of radius" + radius + "is:" + area);
+            Console.WriteLine("Circumference of radius" + radius + "is:" + circumference);
+
+
+            Console.WriteLine("Enter the miles per gallon of gas you used:");
+            double mpg = double.Parse(Console.ReadLine());
+            double galPerCir = (circumference / mpg);
+
+            Console.WriteLine("It takes" + galPerCir + "gallons to drive around a circle with a circumference of" + circumference + "miles.");
+        }
+        
+    }
+}
